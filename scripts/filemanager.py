1d1cabee638b724c38a03f15fdbd250685dbc53a
miyumiyuv <32394524+miyumiyuv@users.noreply.github.com>
2023-12-20 17:14:42 +0300

    Create filemanager.py

---
+++ scripts/filemanager.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import gradio as gr
+
+# Klasör içeriğini listeleme
+def list_files(directory):
+    try:
+        files = os.listdir(directory)
+        return "\n".join(files)
+    except Exception as e:
+        return str(e)
+
+# Dosya kopyalama
+def copy_file(source, destination):
+    try:
+        shutil.copy(source, destination)
+        return f"Kopyalandı: {source} -> {destination}"
+    except Exception as e:
+        return str(e)
+
+# Dosya silme
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        return f"Silindi: {file_path}"
+    except Exception as e:
+        return str(e)
+
+# Dosya taşıma
+def move_file(source, destination):
+    try:
+        shutil.move(source, destination)
+        return f"Taşındı: {source} -> {destination}"
+    except Exception as e:
+        return str(e)
+
+# Gradio arayüzü
+def create_interface():
+    with gr.Blocks() as app:
+        with gr.Tab("File Manager"):
+            with gr.Row():
+                list_button = gr.Button("List Files")
+                directory_input = gr.Textbox(label="Directory", value=".")
+                list_output = gr.Textbox(label="Files", multiline=True, interactive=False)
+                list_button.click(list_files, inputs=[directory_input], outputs=[list_output])
+
+            with gr.Row():
+                copy_source = gr.Textbox(label="Copy - Source File")
+                copy_destination = gr.Textbox(label="Copy - Destination File")
+                copy_button = gr.Button("Copy")
+                copy_output = gr.Textbox(label="Result", interactive=False)
+                copy_button.click(copy_file, inputs=[copy_source, copy_destination], outputs=[copy_output])
+
+            with gr.Row():
+                delete_file_input = gr.Textbox(label="Delete - File Path")
+                delete_button = gr.Button("Delete")
+                delete_output = gr.Textbox(label="Result", interactive=False)
+                delete_button.click(delete_file, inputs=[delete_file_input], outputs=[delete_output])
+
+            with gr.Row():
+                move_source = gr.Textbox(label="Move - Source File")
+                move_destination = gr.Textbox(label="Move - Destination File")
+                move_button = gr.Button("Move")
+                move_output = gr.Textbox(label="Result", interactive=False)
+                move_button.click(move_file, inputs=[move_source, move_destination], outputs=[move_output])
+    return app
+
+app = create_interface()
+
+if __name__ == "__main__":
+    app.launch()
